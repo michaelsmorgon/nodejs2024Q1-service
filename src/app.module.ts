4f1783aacaca9d15ecef3282716e16e227ddfc09
michaelsmorgon <smorgon@int.pl>
2024-03-20 18:08:56 +0100

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { DBModule } from './db/db.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { FavoritesModule } from './favorites/favorites.module';
     TrackModule,
     AlbumModule,
     FavoritesModule,
+    DBModule,
   ],
   controllers: [AppController],
   providers: [AppService],
